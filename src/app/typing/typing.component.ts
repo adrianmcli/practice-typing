a7b89d4a09293b37d41ee95be9c95f678279d468
Adrian Li <li.adrianmc@gmail.com>
2017-01-15 00:43:06 -0500

    add multi-phrase support

--- src/app/typing/typing.component.ts
+++ src/app/typing/typing.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+// TODO - use SM2 algo to choose what phrase to show next:
+//        https://www.supermemo.com/english/ol/sm2.htm
+// TODO - use Damerau-Levenshtein distance to calculate/indicator/quantify
+//        typing inaccuracy:
+//        http://stackoverflow.com/questions/41601087/what-string-distance-algorithm-is-best-for-measuring-typing-accuracy
+
 @Component({
   selector: 'app-typing',
   template: `
-    <div>{{phrase}}</div>
-    <input type="text" [(ngModel)]="typedPhrase"/>
+    <div>{{getPhrase()}}</div>
+    <input
+      type="text"\
+      [(ngModel)]="typedPhrase"
+      (keypress)="handleKeypress($event)"
+      />
     <div class="result" [class.valid]="isCorrect()">{{typedPhrase}}</div>
   `,
   styles: [`
@@ -18,16 +28,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypingComponent implements OnInit {
 
-  phrase: string;
+  phrases: string[];
+  phraseIndex: number;
   typedPhrase: string;
 
   constructor() {
-    this.phrase = `git commit -m "hello world"`;
+    this.phrases = [
+      `git commit -m "hello world"`,
+      `git status`,
+    ];
+    this.phraseIndex = 0;
     this.typedPhrase = `type the above string`;
   }
 
+  getPhrase() {
+    return this.phrases[this.phraseIndex];
+  }
+
+  handleKeypress(e) {
+    if (e.code === `Enter`) {
+      this.typedPhrase = ``;
+      const i = this.phraseIndex;
+      const { phrases } = this;
+      const indexAtLastItem = i >= phrases.length - 1;
+
+      if (indexAtLastItem) {
+        this.phraseIndex = 0;
+      } else {
+        this.phraseIndex = i + 1;
+      }
+    }
+  }
+
   isCorrect() {
-    return this.phrase === this.typedPhrase;
+    return this.getPhrase() === this.typedPhrase;
   }
 
   ngOnInit() {
