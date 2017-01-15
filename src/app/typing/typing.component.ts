fe8348af1a10dde782cb2911571776ca7391b5be
Adrian Li <li.adrianmc@gmail.com>
2017-01-15 02:22:42 -0500

    add ability to add phrases and a timer

--- src/app/typing/typing.component.ts
+++ src/app/typing/typing.component.ts
@@ -9,13 +9,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-typing',
   template: `
-    <div>{{getPhrase()}}</div>
+    <h2>Add Phrase</h2>
+    <input
+      type="text"
+      (keypress)="handleKeypress2($event)"
+      />
+    <h2>List of Phrases</h2>
+    <div>
+      <div *ngFor="let phrase of phrases">
+        {{ phrase }}
+      </div>
+    </div>
+    <h2>Practice</h2>
+    <button
+      *ngIf="typingStart === false"
+      (click)="startTypingGame()"
+      >
+      Start
+    </button>
+    <div>{{ getPhrase() }}</div>
     <input
       type="text"\
       [(ngModel)]="typedPhrase"
       (keypress)="handleKeypress($event)"
       />
-    <div class="result" [class.valid]="isCorrect()">{{typedPhrase}}</div>
+    <div class="result" [class.valid]="isCorrect()">{{ typedPhrase }}</div>
   `,
   styles: [`
     .result {
@@ -31,6 +49,7 @@ export class TypingComponent implements OnInit {
   phrases: string[];
   phraseIndex: number;
   typedPhrase: string;
+  typingStart: boolean;
 
   constructor() {
     this.phrases = [
@@ -39,15 +58,29 @@ export class TypingComponent implements OnInit {
     ];
     this.phraseIndex = 0;
     this.typedPhrase = `type the above string`;
+    this.typingStart = false;
+  }
+
+  startTypingGame() {
+    this.typingStart = true;
+    setTimeout(() => {
+      this.typingStart = false;
+    }, 2000);
   }
 
   getPhrase() {
     return this.phrases[this.phraseIndex];
   }
 
+  handleKeypress2(e) {
+    if (e.code === `Enter`) {
+      this.phrases.push(e.target.value);
+      e.target.value = ``;
+    }
+  }
+
   handleKeypress(e) {
     if (e.code === `Enter`) {
-      this.typedPhrase = ``;
       const i = this.phraseIndex;
       const { phrases } = this;
       const indexAtLastItem = i >= phrases.length - 1;
@@ -57,6 +90,7 @@ export class TypingComponent implements OnInit {
       } else {
         this.phraseIndex = i + 1;
       }
+      this.typedPhrase = ``;
     }
   }
 
